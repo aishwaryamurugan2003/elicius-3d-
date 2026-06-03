07871307a73fffb2c93e06a47eb35c4998c37a18
aishwarya <aishwaryamurugan2003@gmail.com>
2026-06-03 16:35:31 +0530

    career

--- src/pages/Career.tsx
+++ src/pages/Career.tsx
@@ -207,7 +207,7 @@ const ApplicationModal = ({
     fd.append("resume",        selectedFile);
 
     try {
-      const res  = await fetch("http://localhost:8000/apply/", { method: "POST", body: fd });
+      const res  = await fetch("/apply/", { method: "POST", body: fd });
       const data = await res.json();
       if (data.status === "success") {
         setSubmitted(true);
